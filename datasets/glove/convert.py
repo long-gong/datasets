ab7ecc635cdcdd923b5add9d8bb4d0e0f9055955
Long Gong <long.github@gmail.com>
2019-12-28 15:19:13 -0500

    convert.py for glove: change back to python2

--- datasets/glove/convert.py
+++ datasets/glove/convert.py
@@ -1,15 +1,27 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 import sys
 import struct
 import numpy as np
+import sys
+if sys.version_info[0] != 2:
+    raise Exception("Must be using Python 2")
 
 matrix = []
 with open('glove.twitter.27B.100d.txt', 'r') as inf:
     with open('glove.twitter.27B.100d.dat', 'wb') as ouf:
         counter = 0
         for line in inf:
+            # tmp = line.split()[0]
+            # try:
+            #     x = float(tmp)
+            #     print(f"{tmp} {counter + 1}")
+            #     exit(0)
+            # except:
+            #     pass
             row = [float(x) for x in line.split()[1:]]
+            # if len(row) != 100:
+            #     print(f"{line} {len(row)}")
             assert len(row) == 100
             ouf.write(struct.pack('i', len(row)))
             ouf.write(struct.pack('%sf' % len(row), *row))
