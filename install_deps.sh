382cc0764491a9c25e3149fad27eaf89b6af1106
Long Gong <longgong@localhost.localdomain>
2020-01-03 16:57:07 -0500

    added support for Fedora

--- install_deps.sh
+++ install_deps.sh
@@ -1,6 +1,42 @@
 #!/usr/bin/env bash
 
-sudo apt-get install libhdf5-dev libeigen3-dev libhdf5-mpi xtensor
+# Dataset dependency install script for Ubuntu and Fedora
+
+# Fail on error
+set -e 
+
+# Echo on
+set -x 
+
+# Fail on unset var usage
+set -o nounset
+
+# Attempt to install command
+
+DIST=Unknown
+
+test -e /etc/debian_version && DIST="Debian"
+grep Ubuntu /etc/lsb-release &> /dev/null && DIST="Ubuntu"
+if [ "$DIST" = "Ubuntu" ] || [ "$DIST" = "Debian" ]; then
+    # Truly non-interactive apt-get installation
+    install='sudo DEBIAN_FRONTEND=noninteractive apt-get -y -q install'
+    $install libhdf5-dev libeigen3-dev 
+fi
+test -e /etc/fedora-release && DIST="Fedora"
+if [ "$DIST" = "Fedora" ]; then
+    install='sudo dnf -y install'
+    $install hdf5-devel eigen3-devel 
+fi
+test -e /etc/redhat-release && DIST="RedHatEnterpriseServer"
+if [ "$DIST" = "RedHatEnterpriseServer" ]; then
+    install='sudo dnf -y install'
+    # TODO
+fi
+test -e /etc/SuSE-release && DIST="SUSE Linux"
+if [ "$DIST" = "SUSE Linux" ]; then
+    install='sudo zypper --non-interactive install '
+    # TODO
+fi
 
 cd /tmp
 rm -rf HighFive xxHash
